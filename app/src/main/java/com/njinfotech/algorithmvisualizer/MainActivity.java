97f4429793b8f220999f431af35ff2462e28370f
hkourtev <hristiyan@kourtev.com>
2015-11-12 19:09:23 -0500

    Added blank activities for Test, Learn, Stats

--- app/src/main/java/com/njinfotech/algorithmvisualizer/MainActivity.java
+++ app/src/main/java/com/njinfotech/algorithmvisualizer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.njinfotech.algorithmvisualizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLearnAct(View view) {
+        Intent intent = new Intent(this, LearnActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.btnGoToLearnAct);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void goToTestAct(View view) {
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToStatsAct(View view) {
+        Intent intent = new Intent(this, StatsActivity.class);
+        startActivity(intent);
+    }
+
+    public void exit(View view) {
+        // exit
     }
 }
