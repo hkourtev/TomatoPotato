e15256491765c7429acc3b1e869c179c0254226a
Aditya <ganeshaditya1@gmail.com>
2015-11-22 21:08:30 -0500

    Fixes the problem with redrawing canvas.

--- app/src/main/java/com/njinfotech/algorithmvisualizer/Graph.java
+++ app/src/main/java/com/njinfotech/algorithmvisualizer/Graph.java
@@ -242,11 +242,15 @@ public class Graph {
     // draws the graph with the provided edges
     public void draw(Edge[] edges) {
         // blank the canvas
-        //drawSpace.invalidate();
+        canvas.drawColor(Color.WHITE);
+        for(Node n: nodes){
+            n.draw();
+        }
+        /*
         Paint line = new Paint();
         line.setColor(0xFFFFFFFF);
         canvas.drawRect(0,0,screenSize.x-200, screenSize.y,line);
-
+    */
         // draw all nodes
         for (int i=0; i<nodes.length; i++) {
             nodes[i].draw();
